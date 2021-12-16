62e0c30a0cf16467e6644b9e83a1e8593d2eaf48
BAZBOT123 <hughesy1993@hotmail.co.uk>
2021-12-16 16:17:45 +0000

    Still working on soft-play.spec.js

--- src/soft-play.js
+++ src/soft-play.js
@@ -10,11 +10,27 @@ let children = 0
 
 // TODO: Write your functions in the below section. Your functions should update
 // the adults and children variables defined above.
+//
+function enter (numChildren, numAdults) {
+  adults += numAdults
+  children += numChildren
+  return true
+}
+function leave (numChildren, numAdults) {
+  adults += numAdults
+  children -= numChildren
+  return false
+}
+
+
+
+
+
 
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
-  enter: undefined,
-  leave: undefined,
+  enter: enter,
+  leave: leave,
   occupancy: undefined,
   reset: reset
 }
